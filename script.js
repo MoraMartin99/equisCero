edffc7413af656b90bfe598623976ff16140e6bd
Oscar Martin Mora <moramartin99@gmail.com>
2023-11-09 15:02:18 -0600

    Add setPlayer to game

--- script.js
+++ script.js
@@ -17,6 +17,42 @@ const game = (() => {
         return { getToken };
     })();
 
+    const setPlayer = (name) => {
+        const playerArr = _players;
+        const createPlayer = (name) => {
+            let _id, _token, _avatarImageURL;
+            const _name = name;
+            const getName = () => {
+                return _name;
+            };
+            const getId = () => {
+                return _id;
+            };
+            const getToken = () => {
+                return _token;
+            };
+            const getAvatarImageURL = () => {
+                return _avatarImageURL;
+            };
+
+            if (!playerArr.length) {
+                _id = "player1";
+                _token = _tokens.getToken("token1");
+                _avatarImageURL; //aquí debe ocurrir alguna lógica que obtenga el recurso de externalResource
+            } else {
+                _id = "player2";
+                _token = _tokens.getToken("token2");
+                _avatarImageURL; //aquí debe ocurrir alguna lógica que obtenga el recurso de externalResource
+            }
+
+            return { getName, getId, getToken, getAvatarImageURL };
+        };
+
+        if (playerArr.length < 2) {
+            playerArr.push(createPlayer(name));
+        }
+    };
+
     return {
         setPlayer,
         start,
