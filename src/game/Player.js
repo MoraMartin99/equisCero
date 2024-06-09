8fbe670e06bb6a4021022aca7ca2d7c2ee3b1734
Oscar Martin Mora <moramartin99@gmail.com>
2024-06-09 16:04:52 -0600

    Add game/Player.js

---
+++ src/game/Player.js
@@ -0,0 +1,30 @@
+export default class Player {
+    #properties;
+    #avatarSources;
+
+    constructor({ id, name, role, avatarSources, token, order }) {
+        this.#avatarSources = avatarSources;
+        const getSource = (sourceId) => Object(this.#avatarSources).getSource?.(sourceId);
+
+        this.#properties = {
+            id,
+            name,
+            role,
+            token,
+            order,
+            get avatarSource() {
+                if (role === "CPU") return getSource("CPU");
+                if (role === "user") return getSource("player1");
+                return getSource(id);
+            },
+        };
+    }
+
+    getProperty(name) {
+        return this.#properties[name];
+    }
+
+    getAllProperties() {
+        return { ...this.#properties };
+    }
+}
