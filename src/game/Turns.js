dfb5720c66759e5da8f71e14dd687f4911b5c6f6
Oscar Martin Mora <moramartin99@gmail.com>
2024-06-11 16:03:36 -0600

    Add game/Turns.js

---
+++ src/game/Turns.js
@@ -0,0 +1,34 @@
+export default class Turns {
+    #playersIdList;
+    #queue = [];
+
+    setPlayersIdList(playersIdList) {
+        this.#playersIdList = playersIdList;
+    }
+
+    getCurrentPlayerId() {
+        return this.#queue[this.#queue.length - 1];
+    }
+
+    nextTurn() {
+        if (!this.#playersIdList.length) return;
+        if (this.#queue[this.#queue.length - 1] === this.#playersIdList[0]) {
+            this.#queue.push(this.#playersIdList[1]);
+        } else {
+            this.#queue.push(this.#playersIdList[0]);
+        }
+    }
+
+    getCurrentTurn() {
+        return this.#queue.length;
+    }
+
+    resetQueue() {
+        this.#queue = [];
+    }
+
+    reset() {
+        this.resetQueue();
+        this.#playersIdList = undefined;
+    }
+}
