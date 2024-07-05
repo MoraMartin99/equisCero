c36d15e5f89fa6bb656b20ef7a12575900da1e8f
Oscar Martin Mora <moramartin99@gmail.com>
2024-07-05 16:45:37 -0600

    Rewrite Type class to work with GameController class

--- src/game/Type.js
+++ src/game/Type.js
@@ -1,14 +1,8 @@
 export default class Type {
     #currentType;
-    #typeList = ["PVSP", "PVSCPU"];
-
-    #isValidType(type) {
-        return this.#typeList.includes(type);
-    }
 
     setType(type) {
-        type = String(type).toUpperCase();
-        if (this.#isValidType(type)) this.#currentType = type;
+        this.#currentType = String(type);
     }
 
     getType() {
