b288b2f7e71ebd7aaa0000b262c87a8421e7a3f4
Oscar Martin Mora <moramartin99@gmail.com>
2024-07-16 16:29:27 -0600

    Add display/ClassObserver.js

---
+++ src/display/ClassObserver.js
@@ -0,0 +1,37 @@
+import StateObserver from "./StateObserver.js";
+import Elements from "./Elements.js";
+import { isIterable } from "../Utilities.js";
+
+const { isHTMLElement } = Elements;
+
+export default class ClassObserver extends StateObserver {
+    observe(element, properties, maxTolerableTime) {
+        const { classList } = Object(properties);
+        const hasValidArguments = () => {
+            if (!isHTMLElement(element)) return false;
+            if (!isIterable(classList) || typeof classList === "string") return false;
+            return true;
+        };
+        let mutationObserver;
+        maxTolerableTime = !Number.isFinite(maxTolerableTime)
+            ? super.minTime
+            : maxTolerableTime > 0
+            ? maxTolerableTime * 1000
+            : super.minTime;
+
+        const promise = new Promise((resolve, reject) => {
+            if (!hasValidArguments()) return reject("invalid arguments");
+            const callback = ([{ target }]) => {
+                const hasAllClasses = (target, classList) => {
+                    return Array.from(classList).every((currentClass) => target.classList.contains(currentClass));
+                };
+                if (hasAllClasses(target, classList)) resolve({ element, properties });
+            };
+            mutationObserver = new MutationObserver(callback);
+            mutationObserver.observe(element, { attributeFilter: ["class"] });
+            setTimeout(() => reject("maxTolerableTime reached"), maxTolerableTime);
+        });
+
+        return promise.finally(() => Object(mutationObserver).disconnect?.());
+    }
+}
