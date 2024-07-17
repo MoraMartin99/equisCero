9b234f3f63de4fb345156ec2340fddc2c560a9b3
Oscar Martin Mora <moramartin99@gmail.com>
2024-07-17 16:50:47 -0600

    Upgrade display/StateObserver.js

--- src/display/StateObserver.js
+++ src/display/StateObserver.js
@@ -1,7 +1,13 @@
 export default class StateObserver {
+    #minTime;
+    constructor(minTime) {
+        const baseMinTime = 10 * 1000;
+        this.#minTime = !Number.isFinite(minTime) ? baseMinTime : minTime > 0 ? minTime * 1000 : baseMinTime;
+    }
+
     observe(element, properties, maxTolerableTime) {}
 
     get minTime() {
-        return 10 * 1000;
+        return this.#minTime;
     }
 }
