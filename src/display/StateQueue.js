7d865e5d654792d7d59ea69da6da4c3910985b0c
Oscar Martin Mora <moramartin99@gmail.com>
2024-07-17 16:53:46 -0600

    Add display/StateQueue.js

---
+++ src/display/StateQueue.js
@@ -0,0 +1,18 @@
+import { isIterable } from "../Utilities.js";
+
+export default class StateQueue {
+    #queue = Promise.resolve();
+
+    add(stateList, maxTolerableTime) {
+        if (isIterable(stateList) && typeof stateList !== "string") {
+            const mapper = (item) => {
+                const { state, element } = Object(item);
+                return Promise.resolve(Object(state).apply?.(element, maxTolerableTime));
+            };
+
+            this.#queue = this.#queue.then(() => Promise.allSettled(Array.from(stateList).map(mapper)));
+        }
+
+        return this.#queue;
+    }
+}
