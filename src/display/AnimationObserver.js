2121215e31868a572342e86e8ea4db5b8239974e
Oscar Martin Mora <moramartin99@gmail.com>
2024-07-21 15:49:02 -0600

    Remove dependencies from display/AnimationObserver.js

--- src/display/AnimationObserver.js
+++ src/display/AnimationObserver.js
@@ -1,14 +1,11 @@
 import StateObserver from "./StateObserver.js";
-import Elements from "./Elements.js";
-
-const { isHTMLElement, updateElement } = Elements;
 
 export default class AnimationObserver extends StateObserver {
     observe(element, properties, maxTolerableTime) {
         const { animationName } = Object(properties);
         const hasValidArguments = () => {
-            if (!isHTMLElement(element)) return false;
             if (typeof animationName !== "string" || !animationName) return false;
+            if (!Boolean(element instanceof Element)) return false;
             return true;
         };
         const event = "animationend";
@@ -18,15 +15,16 @@ export default class AnimationObserver extends StateObserver {
             : maxTolerableTime > 0
             ? maxTolerableTime * 1000
             : super.minTime;
+        element = Object(element);
         const promise = new Promise((resolve, reject) => {
             if (!hasValidArguments()) return reject("invalid arguments");
             handler = (e) => {
                 if (e.animationName === animationName) resolve({ element, properties });
             };
-            updateElement(element, { events: { itemsToAdd: [{ event, handler }] } });
+            element.addEventListener?.(event, handler);
             setTimeout(() => reject("maxTolerableTime reached"), maxTolerableTime);
         });
 
-        return promise.finally(() => updateElement(element, { events: { itemsToRemove: [{ event, handler }] } }));
+        return promise.finally(() => element.removeEventListener?.(event, handler));
     }
 }
