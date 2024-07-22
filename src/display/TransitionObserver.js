13f54809cca947b301476710e8cef498f36548db
Oscar Martin Mora <moramartin99@gmail.com>
2024-07-22 15:19:50 -0600

    Remove dependencies from display/TransitionObserver.js

--- src/display/TransitionObserver.js
+++ src/display/TransitionObserver.js
@@ -1,14 +1,11 @@
 import StateObserver from "./StateObserver.js";
-import Elements from "./Elements.js";
 import { isIterable } from "../Utilities.js";
-
-const { isHTMLElement, updateElement } = Elements;
 
 export default class TransitionObserver extends StateObserver {
     observe(element, properties, maxTolerableTime) {
         const { propertyNameList } = Object(properties);
         const hasValidArguments = () => {
-            if (!isHTMLElement(element)) return false;
+            if (!Boolean(element instanceof Element)) return false;
             if (!isIterable(propertyNameList) || typeof propertyNameList === "string") return false;
             return true;
         };
@@ -19,18 +16,20 @@ export default class TransitionObserver extends StateObserver {
             : maxTolerableTime > 0
             ? maxTolerableTime * 1000
             : super.minTime;
+        element = Object(element);
+
         const promise = new Promise((resolve, reject) => {
             if (!hasValidArguments()) return reject("invalid arguments");
-            let propertyNameListCopy = [...Array.from(propertyNameList)];
+            let propertyNameListCopy = Array.from(propertyNameList);
             handler = (e) => {
                 const propertyName = e.propertyName;
                 propertyNameListCopy = propertyNameListCopy.filter((item) => item !== propertyName);
                 if (!propertyNameListCopy.length) resolve({ element, properties });
             };
-            updateElement(element, { events: { itemsToAdd: [{ event, handler }] } });
+            element.addEventListener(event, handler);
             setTimeout(() => reject("maxTolerableTime reached"), maxTolerableTime);
         });
 
-        return promise.finally(() => updateElement(element, { events: { itemsToRemove: [{ event, handler }] } }));
+        return promise.finally(() => element.removeEventListener?.(event, handler));
     }
 }
