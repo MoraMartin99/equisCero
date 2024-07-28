edb4093d401b65dc837e987182e18f0906808432
Oscar Martin Mora <moramartin99@gmail.com>
2024-07-28 10:12:12 -0600

    Add display/SessionForm.js

---
+++ src/display/SessionForm.js
@@ -0,0 +1,80 @@
+import { isIterable } from "../Utilities.js";
+
+export default class SessionForm {
+    #element;
+    #event;
+    #nextScreenCallback;
+    #previousScreenCallback;
+    #fieldsExtractorCallback;
+    #previousScreenTriggerList;
+    #nextScreenTriggerList;
+    #playerNameInputList;
+    #avatarManager;
+
+    constructor(settings) {
+        const {
+            element,
+            event,
+            nextScreenCallback,
+            previousScreenCallback,
+            fieldsExtractorCallback,
+            previousScreenTriggerList,
+            nextScreenTriggerList,
+            playerNameInputList,
+            avatarManager,
+        } = Object(settings);
+
+        this.#element = Object(element);
+        this.#event = Object(event);
+        this.#nextScreenCallback = nextScreenCallback;
+        this.#previousScreenCallback = previousScreenCallback;
+        this.#fieldsExtractorCallback = fieldsExtractorCallback;
+        this.#previousScreenTriggerList = isIterable(previousScreenTriggerList) ? Array.from(previousScreenTriggerList) : [];
+        this.#nextScreenTriggerList = isIterable(nextScreenTriggerList) ? Array.from(nextScreenTriggerList) : [];
+        this.#playerNameInputList = isIterable(playerNameInputList) ? Array.from(playerNameInputList) : [];
+        this.#avatarManager = Object(avatarManager);
+
+        this.#element.addEventListener?.("click", this.#clickHandler);
+    }
+
+    getElement() {
+        return this.#element;
+    }
+
+    reset() {
+        this.#element.reset?.();
+        this.#playerNameInputList.forEach((item) => Object(item).restart?.());
+        this.#avatarManager.reset?.();
+    }
+
+    setPlayerNamePattern(pattern) {
+        this.#playerNameInputList.forEach((item) => Object(item).setPattern?.(pattern));
+    }
+
+    removePlayerNamePatter() {
+        this.#playerNameInputList.forEach((item) => Object(item).reset?.());
+    }
+
+    setAvatarSources(src, resourceId) {
+        this.#avatarManager.setSources?.(src, resourceId);
+    }
+
+    #clickHandler = (e) => {
+        const target = e.target;
+        const containsTarget = (item) => Boolean(Object(item).contains?.(target));
+
+        if (this.#previousScreenTriggerList.some(containsTarget)) {
+            this.#previousScreenCallback();
+        }
+
+        if (this.#nextScreenTriggerList.some(containsTarget)) {
+            if (this.#element.checkValidity?.()) {
+                this.#event.trigger?.({
+                    senderId: this.#element.id,
+                    fields: Object(this.#fieldsExtractorCallback(this.#element)),
+                });
+                this.#nextScreenCallback();
+            }
+        }
+    };
+}
