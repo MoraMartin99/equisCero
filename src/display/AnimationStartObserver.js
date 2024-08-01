771cef9a46bf4312d03087bd8ba537cc1f3205b7
Oscar Martin Mora <moramartin99@gmail.com>
2024-08-01 16:34:49 -0600

    Add display/AnimationStartObserver.js

---
+++ src/display/AnimationStartObserver.js
@@ -0,0 +1,30 @@
+import StateObserver from "./StateObserver.js";
+
+export default class AnimationStartObserver extends StateObserver {
+    observe(element, properties, maxTolerableTime) {
+        const { animationName } = Object(properties);
+        const hasValidArguments = () => {
+            if (typeof animationName !== "string" || !animationName) return false;
+            if (!Boolean(element instanceof Element)) return false;
+            return true;
+        };
+        const event = "animationstart";
+        let handler;
+        maxTolerableTime = !Number.isFinite(maxTolerableTime)
+            ? super.minTime
+            : maxTolerableTime > 0
+            ? maxTolerableTime * 1000
+            : super.minTime;
+        element = Object(element);
+        const promise = new Promise((resolve, reject) => {
+            if (!hasValidArguments()) return reject("invalid arguments");
+            handler = (e) => {
+                if (e.animationName === animationName) resolve({ element, properties });
+            };
+            element.addEventListener?.(event, handler);
+            setTimeout(() => reject("maxTolerableTime reached"), maxTolerableTime);
+        });
+
+        return promise.finally(() => element.removeEventListener?.(event, handler));
+    }
+}
