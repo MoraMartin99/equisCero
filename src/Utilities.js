d330e809e8c1397dd72a8715a2c570ecfe8c274f
Oscar Martin Mora <moramartin99@gmail.com>
2024-10-25 15:45:03 -0600

    Fix missing parameter in getRandomItemByWeight

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,6 +1,9 @@
 const isFunction = (callback) => typeof callback === "function";
-const getRandomItemByWeight = () => {
-    const totalWeight = weightList.reduce((total, { weight }) => (total += weight), 0);
+const getRandomItemByWeight = (weightList) => {
+    const totalWeight = weightList.reduce(
+        (total, { weight }) => (total += weight),
+        0
+    );
     const randomNumber = Math.random() * totalWeight;
     let accumulatedWeight = 0;
     for (const { item, weight } of weightList) {
